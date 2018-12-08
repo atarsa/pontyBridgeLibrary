3f26739ce1fd41ee58253c6406d99d72748d08e5
ATarsa <smb909@gmail.com>
2018-12-08 11:44:47 +0000

    Feature: add modify user function
    Basic functionality. Will need refactoring

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,7 @@ var search_url = "/search";
 // --- Search Page ---
 const searchForm = document.querySelector('.search-form');
 const searchResults = document.querySelector('.search-results');
+const messageDiv = document.querySelector('.message');
 
 
 
@@ -58,10 +59,12 @@ getData(users_url)
     }
   })
 
-// SEARCH functions
+// SEARCH, UPDATE event listeneres 
 searchForm.addEventListener("submit", search);
-    
- 
+searchResults.addEventListener("click", editElement);    
+
+
+// SEARCh, UPDATE functions
 function search(e){
   let queryUrl = base_url+search_url;
   // get values from search form
@@ -96,25 +99,34 @@ function showResults(results) {
   document.getElementById('search').value = "";
   if (results.length !== 0){
     for (let result of results){
+      let id = result.id;
       console.log(result);
       let li = document.createElement('li');
       // show results for user
       if ('name' in result){
-        li.innerHTML = `${result.name}, ${result.barcode}, ${result.memberType}`;
+        // add data atribute to identify item in db
+        li.setAttribute("data-userId", id);
+        li.innerHTML = `<span>${result.name}</span>
+                        <span>${result.barcode}</span>
+                        <span> ${result.memberType}</span>`;
         const updateElm = document.createElement('a');
+        updateElm.setAttribute("href", "#");
         updateElm.innerHTML = '<i class="fas fa-pen-square"></i>';
         updateElm.classList = "update-user";
         
         const deleteElm = document.createElement('a');
+        deleteElm.setAttribute("href", "#");
         deleteElm.innerHTML = '<i class="fas fa-trash-alt"></i>';
         deleteElm.classList = "delete-user";
         li.appendChild(updateElm);
         li.appendChild(deleteElm);
       
       } else { // show results for books
+        li.setAttribute("data-bookId", id);
         li.innerHTML = `${result.title}, ${result.isbn}`;
         
         const deleteElm = document.createElement('a');
+        deleteElm.setAttribute("href", "#");
         deleteElm.innerHTML = '<i class="fas fa-trash-alt"></i>';
         deleteElm.classList = "delete-book";
         li.appendChild(deleteElm);
@@ -129,11 +141,94 @@ function showResults(results) {
   }
   
 }
+function editElement(e){
+  if(e.target.parentElement.matches('.update-user')){
+    //console.log("update");
+    //console.log(e.target.parentElement.parentElement)
+    updateUser(e.target.parentElement.parentElement);
+  } 
+  else if(e.target.parentElement.matches('.delete-user')){
+    console.log("delete user");
+    console.log(e.target.parentElement.parentElement)
+  } else if(e.target.parentElement.matches('.delete-book')){
+    console.log("delete book");
+    console.log(e.target.parentElement.parentElement)
+  } 
+} 
+
+
+
+function updateUser(target){
+  // remove Event Listener to prevent creating new form with every click
+  searchResults.removeEventListener("click", editElement);
+  console.log(target);
+  // Get element ID
+  console.log(target.attributes[0].value);
+  let id = target.attributes[0].value;
   
-function updateUser(e){
-  console.log();
+  // create div with form to update user details
+  const div = document.createElement('div');
+  div.setAttribute("class", "update-user");
+  
+  //console.log(target.children[0].innerText);
+  //console.log(target.children[2].innerText);
+  const inputName = document.createElement("input");
+  inputName.setAttribute("type", "text");
+  inputName.value = target.children[0].innerText;
+  
+  const inputMemberType = document.createElement("select");
+  const inputMemberTypeStudent = document.createElement("option");
+  inputMemberTypeStudent.setAttribute("value","student");
+  inputMemberTypeStudent.innerText = "Student";
+
+  const inputMemberTypeStaff = document.createElement("option");
+  inputMemberTypeStaff.setAttribute("value","staff");
+  inputMemberTypeStaff. innerText = "Staff";
+  inputMemberType.appendChild(inputMemberTypeStudent);
+  inputMemberType.appendChild(inputMemberTypeStaff);
+  
+  const submitBtn = document.createElement('input');
+  submitBtn.setAttribute("type", "submit");
+  submitBtn.setAttribute("value", "Submit");
+
+  div.appendChild(inputName);
+  div.appendChild(inputMemberType);
+  div.appendChild(submitBtn);
+
+  target.appendChild(div);
+
+  submitBtn.addEventListener("click", function(){
+    //confirm("Are you sure you want to update this user?")
+    let queryUrl = `${base_url}${users_url}/${id}`;
+    let updatedData = {name: inputName.value,
+                      memberType: inputMemberType.value}
+
+    console.log("updating...");
+    console.log(queryUrl);
+    // PUT request to update data on the server
+    fetch(queryUrl, {
+      method: "PUT",
+      body: JSON.stringify(updatedData),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(error => console.error('Error:', error));
+
+    // Message if successfull
+    searchResults.innerHTML = "";
+    messageDiv.style.background = "green";
+    messageDiv.innerText = "User modified successfully";
+    setTimeout(function(){
+      messageDiv.innerHTML = "";}, 3000);
+    
+  });
+ 
 }
 
+
+
 function deleteUser(e){
-  alert("Do you realy want me go?");
+  confirm("Are you sure you want to delete this item?");
 }
