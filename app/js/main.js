3330844e9c45c8c59ea51563e292007093dd60a7
ATarsa <smb909@gmail.com>
2018-12-15 11:28:56 +0000

    Bug: Fixed items count for homepage

--- app/js/main.js
+++ app/js/main.js
@@ -7,34 +7,61 @@ var books_url = "/books";
 var search_url = "/search";
 var authors_url = "/authors"
 
+// get the current location
+var currentLocation = window.location.pathname;
+
+
 // UI elements
 // --- HomePage ---
-
-
-
-
+let books_count = document.getElementById('books-count');
+let users_count = document.getElementById('users-count');
 
 
 // // get Books and Users count for HomePage
 // async function getHomePageData(){
   
 //   try{
+//     console.log("fetching data...")
 //     const booksResponse = await fetch(base_url+books_url);
 //     const usersResponse = await fetch(base_url+users_url);
   
 //     const booksData = await booksResponse.json();
 //     const usersData = await usersResponse.json();
-  
-//     let books_count = document.getElementById('books-count');
-//       books_count.innerHTML= booksData.length;
-  
-//     let users_count = document.getElementById('users-count');
+    
+//     console.log(booksData);
+    
+//     books_count.innerHTML= booksData.length;
 //     users_count.innerHTML= usersData.length;
 //   } catch(err){
 //     console.log(err);
 //   }
 // }
-// getHomePageData();
+
+function getHomePageData(){
+  
+    try{
+      console.log("fetching data...")
+     
+        getData(books_url).then(books => {
+          console.log(books)
+          books_count.innerHTML= books.length;
+        })
+      
+      
+        getData(users_url).then(users =>{
+          users_count.innerHTML= users.length;
+        })
+     }
+       
+    catch(err){
+      console.log(err);
+    }
+  }
+// if on homePage show all records counts
+if (currentLocation == "/index.html" || currentLocation == "/" ){
+  getHomePageData();
+}
+
 
 // All users
 async function getData(query){
